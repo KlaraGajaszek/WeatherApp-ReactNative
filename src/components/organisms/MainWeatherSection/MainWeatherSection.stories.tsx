4893a37a46e0f94a6c0bf655c9bb4f33a58a9b98
KlaraGajaszek <klaragajaszek21@wp.pl>
2022-09-21 14:28:42 +0200

    feat: add stories to MainweatherSection component

---
+++ src/components/organisms/MainWeatherSection/MainWeatherSection.stories.tsx
@@ -0,0 +1,11 @@
+import { MainWeatherSection } from '.';
+
+export default {
+  title: 'components/organisms',
+  component: MainWeatherSection,
+  argTypes: {},
+};
+
+export const WeatherSection = () => <MainWeatherSection />;
+
+WeatherSection.args = {};
