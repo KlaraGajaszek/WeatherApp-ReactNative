99794d1b308b1d46d07d1044fc989eacec409b1a
KlaraGajaszek <klaragajaszek21@wp.pl>
2022-09-23 09:25:50 +0200

    test: add test for ErrorContainer

--- src/components/atoms/ErrorContainer/ErrorContainer.test.tsx
+++ src/components/atoms/ErrorContainer/ErrorContainer.test.tsx
@@ -1,3 +1,4 @@
+import { ErrorTextMessage } from '../../../constants/errors';
 import { render } from '../../../test-utils';
 import { ErrorContainer } from './ErrorContainer';
 
@@ -5,8 +6,12 @@ describe('Error Container', () => {
   it('render correctly', () => {
     const { getByText } = render(<ErrorContainer errorMessage={'error message'} />);
 
+    const errorDescription = `${ErrorTextMessage.description} error message`;
+
     const title = getByText('Whoops, something went wrong');
+    const description = getByText(errorDescription);
 
     expect(title).toBeTruthy();
+    expect(description).toBeTruthy();
   });
 });
